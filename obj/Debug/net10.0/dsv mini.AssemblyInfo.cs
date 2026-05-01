6eecf051a8eb7055e961ce6a9d4cb43ca1361269
Silse-ul <siu002@edu.zealand.dk>
2026-05-01 09:25:45 +0200

    jaaaaaaaa

--- obj/Debug/net10.0/dsv mini.AssemblyInfo.cs
+++ obj/Debug/net10.0/dsv mini.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dsv mini")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7daad8e50e8ed90c177529fe39aaec883da850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5f0115adc296585a89931be8d9ca5c2de565d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("dsv mini")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dsv mini")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
